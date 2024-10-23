9ab690b6a9f2b07827e94ffff59f93c7b6f694cb
cjvanlissa <c.j.vanlissa@tilburguniversity.edu>
2024-10-23 10:26:42 +0200

    preprocessed data

---
+++ prepare_data.R
@@ -0,0 +1,14 @@
+# In this file, write the R-code necessary to load your original data file
+# (e.g., an SPSS, Excel, or SAS-file), and convert it to a data.frame. Then,
+# use the function open_data(your_data_frame) or closed_data(your_data_frame)
+# to store the data.
+
+library(worcs)
+library(tidySEM)
+df <- read.csv("penguins.csv", stringsAsFactors = FALSE)
+df[["sex"]] <- NULL
+desc <- descriptives(df)
+cats <- desc$name[desc$type == "character"]
+df[cats] <- lapply(df[cats], factor)
+
+closed_data(df)
